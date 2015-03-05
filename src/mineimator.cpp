3081b549ef8fc33b4ae9fa213c82031d59ca7303
David <david.norgren@hotmail.com>
2015-03-05 12:44:08 +0100

    try clicking

--- src/mineimator.cpp
+++ src/mineimator.cpp
@@ -1,5 +1,7 @@
 #include "mineimator.h"
 
+double colR, colG, colB;
+
 int main() {
 	GLFWwindow* window;
 	glfwSetErrorCallback(errorCallback);
@@ -19,6 +21,11 @@ int main() {
 	glfwSwapInterval(1);
 
 	glfwSetKeyCallback(window, keyCallback);
+	glfwSetMouseButtonCallback(window, mouseCallback);
+	colR = 1;
+	colG = 1;
+	colB = 1;
+
 
 	while (!glfwWindowShouldClose(window)) {
 		float ratio;
@@ -39,11 +46,11 @@ int main() {
 		glRotatef((float)glfwGetTime() * 10.f, 0.f, 0.f, 1.f);
 
 		glBegin(GL_TRIANGLES);
-		glColor3f(1.f, 0.f, 0.f);
+		glColor3f(colR, colG, colB);
 		glVertex3f(-0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 1.f, 0.f);
+		glColor3f(colR, colG, colB);
 		glVertex3f(0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 0.f, 1.f);
+		glColor3f(colR, colG, colB);
 		glVertex3f(0.f, 0.6f, 0.f);
 		glEnd();
 
@@ -64,6 +71,14 @@ void errorCallback(int error, const char* description) {
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void mouseCallback(GLFWwindow* window, int button, int action, int mods) {
+	if (action == GLFW_PRESS) {
+		colR = rand() / (double)RAND_MAX;
+		colG = rand() / (double)RAND_MAX;
+		colB = rand() / (double)RAND_MAX;
+	}
 }
 
 void windowMaximize(GLFWwindow* window) {
--- src/mineimator.h
+++ src/mineimator.h
@@ -8,6 +8,7 @@
 // GLFW
 void errorCallback(int error, const char* description);
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void mouseCallback(GLFWwindow* window, int button, int action, int mods);
 
 // Window
 void windowMaximize(GLFWwindow* window);
